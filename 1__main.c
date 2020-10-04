0786d4257e9341427d38332fe50aa09443017eae
Wojciech Kop <wojciechkoperny@gmail.com>
2020-10-04 21:02:54 +0200

    add new argument to function

--- 1__main.c
+++ 1__main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
 #include <signal.h>
@@ -6,11 +7,13 @@
 #ifndef NULL_PTR
    #define NULL_PTR     ((void *)0u)
 #endif
+#define DEFAULT_TIMER 1U
 static pthread_t thread_fun1, thread_fun2;
+int Timer = DEFAULT_TIMER;
 
 void SigintHandler(int a)
 {
-    printf("BYE World : TERMINATION : %d\n ",a);
+    printf("\nBYE World : TERMINATION : %d\n ",a);
     (void)pthread_cancel(thread_fun1);
     (void)pthread_cancel(thread_fun2);
 }
@@ -22,7 +25,7 @@ void* fun1 (void *arg)
     {
         printf ("\e[1;32mThis is cmake test %d\e[0m\n", ilosc);
         ilosc++;
-        sleep(1);
+        sleep(Timer);
     }
 }
 void* fun2 (void *arg)
@@ -37,6 +40,10 @@ void* fun2 (void *arg)
 }
 int main(int argc, char *argv[])
 {
+    if (argc >1)
+    {
+        Timer = atoi(argv[1]);
+    } 
     printf("Hello World\n");
 
     struct sigaction sa;
