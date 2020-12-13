29f33fd1daa757afaca2d3f563c41f10dcdb9cce
Wojciech Kop <wojciechkoperny@gmail.com>
2020-12-13 16:15:31 +0100

    devide to Threads to library

--- 1__main.cpp
+++ 1__main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
 #include <unistd.h>
 #include <signal.h>
 #include <vector>
-#include <main.hpp>
+#include <vThreads.hpp>
 
 struct sigaction sa;
 
@@ -14,33 +13,7 @@ void *printerFunction(void *a)
     // (void)a;
     std::cout << "\nprinterFunction: " << *(int *)a << "\n";
 }
-void Task::print()
-{
-    std::cout << "print z obiektu\n";
-}
-
-void Task::print2()
-{
-    std::cout << "ciagle zyje\n";
-}
 //*static_cast<std::function<void*()>*>(context);
-Task::Task(std::function<void *(void *)> threadTask, void *arg)
-{
-    std::cout << "hello from contructor\n";
-    m_threadTask = threadTask;
-
-    typedef void *(*function_t)(void *); //http://www.cplusplus.com/forum/general/63552/
-    function_t *fun_ptr = m_threadTask.target<function_t>();
-    (void)pthread_create(&m_threadID, NULL, *fun_ptr, arg);
-    (void)pthread_join(m_threadID, NULL);
-}
-
-Task::~Task()
-{
-    //std::cout << "Hello from destructor!\n";
-    //(void)pthread_cancel(thread_fun1);
-    //(void)pthread_cancel(thread_fun2);
-}
 
 void SigintHandler(int a)
 {
---
+++ VanillaThreads/vThreads.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <vThreads.hpp>
+
+void Task::print()
+{
+    std::cout << "print z obiektu\n";
+}
+
+void Task::print2()
+{
+    std::cout << "ciagle zyje\n";
+}
+//*static_cast<std::function<void*()>*>(context);
+Task::Task(std::function<void *(void *)> threadTask, void *arg)
+{
+    std::cout << "hello from contructor\n";
+    m_threadTask = threadTask;
+
+    typedef void *(*function_t)(void *); //http://www.cplusplus.com/forum/general/63552/
+    function_t *fun_ptr = m_threadTask.target<function_t>();
+    (void)pthread_create(&m_threadID, NULL, *fun_ptr, arg);
+    (void)pthread_join(m_threadID, NULL);
+}
+
+Task::~Task()
+{
+    //std::cout << "Hello from destructor!\n";
+    //(void)pthread_cancel(thread_fun1);
+    //(void)pthread_cancel(thread_fun2);
+}
