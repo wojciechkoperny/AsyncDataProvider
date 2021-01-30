b386c1ecd30944fb72539c9d26c2926e30f340f1
Slawomir Cielepak <slawomir.cielepak@gmail.com>
2021-01-30 23:38:20 +0100

    Cleanup and CMake libraryfor vanilla threads

---
+++ vanillaThreads/include/DataInterface.hpp
@@ -0,0 +1,27 @@
+#ifndef VANILLATHREADS_INCLUDE_DATAINTERFACE
+#define VANILLATHREADS_INCLUDE_DATAINTERFACE
+
+#include <future>
+#include <vector>
+#include <memory>
+
+namespace vanilla::threads
+{
+    /// Forward declaration of Dispatcher
+    class Dispatcher;
+
+    class DataInterface
+    {
+    public:
+        DataInterface();
+        virtual ~DataInterface();
+
+        std::future<std::vector<uint8_t>> RequestDataById(const uint16_t id);
+
+    private:
+        std::unique_ptr<Dispatcher> mDispatcher;
+    };
+
+} // namespace vanilla::threads
+
+#endif /* VANILLATHREADS_INCLUDE_DATAINTERFACE */
---
+++ vanillaThreads/src/Dispatcher.cpp
@@ -0,0 +1,9 @@
+#include "Dispatcher.hpp"
+
+namespace vanilla::threads
+{
+    Dispatcher::Dispatcher() {}
+
+    Dispatcher::~Dispatcher() {}
+
+} // namespace vanilla::threads
